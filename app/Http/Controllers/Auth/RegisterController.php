3dfc1f8b0f00830a084941aea173e3f12055c9f8
ironfalcon <ironfalcon@yandex.ru>
2018-08-19 01:56:41 +0400

    redirect after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin_panel/main';
 
     /**
      * Create a new controller instance.
